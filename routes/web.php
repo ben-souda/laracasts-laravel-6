ce4882be8296be2b2732c026716655c004d2bd3f
adnane <bensouda.adnane.dev@gmail.com>
2022-03-29 17:14:43 +0000

    i understand how to use route and i use arrays

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-   return ['foo' => 'bar'];
+Route::get('/posts/{post}', function($post) {
+   $posts = [
+      'my-first-post' => 'googoogog'
+   ];
+   if (! array_key_exists($post, $posts)){
+      abort(404, 'sorry that post was not found');
+   }
+   return view('post', [
+      'post' => $posts[$post]
+   ]);
 });
+
+
+
+
+
 
 
